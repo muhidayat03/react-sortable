b68ae5525e5c9899c491fa7921c7e587c5a6de2b
Muhammad Hidayat <muhammad.hidayat@stockbit.com>
2022-01-26 15:23:50 +0800

    fix: separate component

---
+++ src/SortableComponent/SortableComponent.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { ReactSortable } from "react-sortablejs";
+
+export const SortableComponent: React.FC = () => {
+  const [sortable, setSortable] = useState(false);
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      text: "Write a cool JS library",
+    },
+    {
+      id: 2,
+      text: "Make it generic enough",
+    },
+    {
+      id: 3,
+      text: "Write README",
+    },
+    {
+      id: 4,
+      text: "Create some examples",
+    },
+    {
+      id: 5,
+      text: "Spam in Twitter",
+    },
+    {
+      id: 6,
+      text: "PROFIT",
+    },
+  ]);
+
+  const toggleSortable = () => setSortable(!sortable);
+
+  const handleSetList = (newState: any) => {
+    setCards(newState);
+  };
+
+  const handleSelect = (event: any) => {
+    console.log(event);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleSortable}>{!sortable ? "Sort" : "Done"}</button>
+      <ReactSortable
+        list={cards}
+        setList={handleSetList}
+        onChoose={handleSelect}
+        animation={500}
+        selectedClass="active"
+        chosenClass="active"
+        disabled={!sortable}
+      >
+        {cards.map((item, index) => (
+          <div key={item.id}>
+            <div
+              className={`card ${sortable && "animation" + ((index % 2) + 1)}`}
+            >
+              {item.text}
+            </div>
+          </div>
+        ))}
+      </ReactSortable>
+    </div>
+  );
+};
+
+export default SortableComponent;
